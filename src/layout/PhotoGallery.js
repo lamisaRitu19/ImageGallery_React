f5a7f2fdcf11881397f58a22fbb7424f21bb0a47
Lamisa Ferdous Ritu <lamisaferdous19@gmail.com>
2023-11-02 16:00:11 +0600

    feat: added delete content on headbar

--- src/layout/PhotoGallery.js
+++ src/layout/PhotoGallery.js
@@ -30,6 +30,18 @@ const PhotoGallery = () => {
 
   return (
     <div className="container bg-white border border-slate-300 rounded-xl drop-shadow mx-auto">
+      {/* <div className="flex justify-between items-center border-b-2 border-slate-300 drop-shadow-sm px-8 py-6">
+        <span className="text-2xl font-bold">
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            className="w-5 h-5 mr-4"
+          />
+          <span>0 File Selected</span>
+        </span>
+        <span className="text-red-700 text-xl font-semibold">Delete Files</span>
+      </div> */}
       <div className="text-2xl font-bold border-b-2 border-slate-300 drop-shadow-sm px-8 py-6">
         Gallery
       </div>
